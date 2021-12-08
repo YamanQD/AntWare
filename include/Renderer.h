a928ff82d9864ad03f192573425cd2facc1b44c7
Yaman Qassas <yaman102011@gmail.com>
2021-12-09 00:02:35 +0200

    Implement Renderer::init()

--- include/Renderer.h
+++ include/Renderer.h
@@ -1,5 +1,6 @@
 #pragma once
-#include<GL/glew.h>
+#include <GL/glew.h>
+#include <Window.h>
 #define RENDERER aw::Renderer::instance()
 namespace aw
 {
--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -10,6 +10,18 @@ void Renderer::init()
 {
     glewExperimental = true;
     glewInit();
+
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glClearDepth(1.0f);
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LEQUAL);
+    glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+    gluPerspective(45.0f, WINDOW.internal.getSize().x / WINDOW.internal.getSize().y, 0.1f, 100.0f);
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
 }
 void Renderer::terminate()
 {
