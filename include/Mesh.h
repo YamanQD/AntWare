bf45edc450c6c1faecaa941259d8301592cf2328
Mohammad Issawi <2000issawi@gmail.com>
2021-12-09 07:45:16 +0200

    Load vertex colors & Add albedo options

--- include/Mesh.h
+++ include/Mesh.h
@@ -10,19 +10,32 @@
 #include <SFML/Graphics/Image.hpp>
 namespace aw
 {
+    enum albedoOptions
+    {
+        TEXTURE,
+        VERTEX_COLORS,
+        UNIFORM_COLOR
+    };
     class Mesh
     {
     private:
         static Assimp::Importer importer;
+        bool hasTexture, hasUniformColor;
+        GLuint texture;
         std::vector<glm::vec3> vertices;
         std::vector<glm::vec3> normals;
         std::vector<glm::vec2> texCoords; //TODO store trasformations in a suitable way
+        std::vector<glm::vec4> colors;
+        glm::vec4 uniformColor;
         void loadTexture(const char *path);
 
     public:
-        Mesh(const char *path);
         Mesh(const char *path, const char *texPath = nullptr);
+        Mesh(const char *path, glm::vec4 color, const char *texPath = nullptr);
         void draw();
-        GLuint texture = 0;
+        bool getTexture(GLuint &texture);
+        void setTexture(GLuint texture);
+        bool getUniformColor(glm::vec4 &color);
+        void setUniformColor(glm::vec4 color);
     };
 }
--- src/Mesh.cpp
+++ src/Mesh.cpp
@@ -20,9 +20,20 @@ Mesh::Mesh(const char *path, const char *texPath)
     {
         loadTexture(texPath);
     }
+    else
+    {
+        hasTexture = false;
+    }
     vertices.resize(mesh->mNumVertices);
     for (unsigned i = 0; i < vertices.size(); ++i)
         vertices[i] = {mesh->mVertices[i].x, mesh->mVertices[i].y, mesh->mVertices[i].z};
+    if (mesh->HasVertexColors(0))
+    {
+        colors.resize(mesh->mNumVertices);
+        for (unsigned i = 0; i < colors.size(); ++i)
+            colors[i] = {mesh->mColors[0][i].r, mesh->mColors[0][i].g,
+                         mesh->mColors[0][i].b, mesh->mColors[0][i].a};
+    }
     if (mesh->HasNormals())
     {
         normals.resize(mesh->mNumVertices);
@@ -37,6 +48,12 @@ Mesh::Mesh(const char *path, const char *texPath)
     }
     importer.FreeScene();
 }
+Mesh::Mesh(const char *path, glm::vec4 color, const char *texPath = nullptr)
+{
+    hasUniformColor = true;
+    uniformColor = color;
+    Mesh(path, texPath);
+}
 void Mesh::loadTexture(const char *path)
 {
     Image image;
@@ -44,9 +61,11 @@ void Mesh::loadTexture(const char *path)
     {
         throw runtime_error(path); //TODO prettier formatting
     }
+    hasTexture = true;
     int imgHeight = image.getSize().y;
     int imgWidth = image.getSize().x;
     const Uint8 *imgData = image.getPixelsPtr();
+    //TODO @YamanQD generate texture
 }
 void Mesh::draw()
 {
@@ -59,4 +78,32 @@ void Mesh::draw()
         glVertex3f(vertices[i].x, vertices[i].y, vertices[i].z);
     }
     glEnd();
+}
+bool Mesh::getTexture(GLuint &texture)
+{
+    if (hasTexture)
+    {
+        texture = this->texture;
+        return true;
+    }
+    return false;
+}
+void Mesh::setTexture(GLuint texture)
+{
+    hasTexture = true;
+    this->texture = texture;
+}
+bool Mesh::getUniformColor(vec4 &color)
+{
+    if (hasUniformColor)
+    {
+        color = this->uniformColor;
+        return true;
+    }
+    return false;
+}
+void Mesh::setUniformColor(vec4 color)
+{
+    hasUniformColor = true;
+    uniformColor = color;
 }
