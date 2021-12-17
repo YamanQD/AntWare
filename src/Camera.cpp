1285c2c5f06c8bdb41dbc293f730a1fd5db2e766
Mohammad Issawi <2000issawi@gmail.com>
2021-12-17 19:34:30 +0200

    Optimize camera updating (ig)

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -8,8 +8,9 @@ Camera::Camera(float FOV, float aspectRatio) : FOV(FOV), aspectRatio(aspectRatio
 
     gluPerspective(FOV, aspectRatio, 0.1f, 100.0f);
 
+    glPushMatrix();
+
     glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
 }
 void Camera::setFOV(float FOV)
 {
@@ -24,18 +25,17 @@ void Camera::setAspectRatio(float aspectRatio)
 void Camera::update()
 {
     vec3 camPos = transform.getPosition();
-    vec3 camDir = vec3(0, 0, 1);
+    vec3 camDir = vec3(0, 0, -1);
     vec3 camUp = vec3(0, 1, 0);
     mat3 rotationMat = mat3(transform.getRotation());
     camDir = rotationMat * camDir;
     camUp = rotationMat * camUp;
     vec3 lookedAt = camDir + camPos;
-    glMatrixMode(GL_PROJECTION); // TODO @YamanQD Optimize if possible ^-^
-    glLoadIdentity();
-    gluPerspective(FOV, aspectRatio, 0.1f, 100.0f);
+    glMatrixMode(GL_PROJECTION);
+    glPopMatrix();
+    glPushMatrix();
     gluLookAt(camPos.x, camPos.y, camPos.z,
               lookedAt.x, lookedAt.y, lookedAt.z,
               camUp.x, camUp.y, camUp.z);
     glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
 }
