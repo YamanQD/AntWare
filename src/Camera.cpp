7c8e1d923f54187d98d1fe38373f7a664b85a53b
Yaman Qassas <yaman102011@gmail.com>
2021-12-17 13:59:25 +0200

    Remove unneccessary code

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -24,18 +24,14 @@ void Camera::setAspectRatio(float aspectRatio)
 void Camera::update()
 {
     vec3 camPos = transform.getPosition();
-    vec3 camDir = vec3(0, 0, 1);
+    vec3 camDir = vec3(0, 0, -1);
     vec3 camUp = vec3(0, 1, 0);
     mat3 rotationMat = mat3(transform.getRotation());
     camDir = rotationMat * camDir;
     camUp = rotationMat * camUp;
     vec3 lookedAt = camDir + camPos;
-    glMatrixMode(GL_PROJECTION); // TODO @YamanQD Optimize if possible ^-^
-    glLoadIdentity();
-    gluPerspective(FOV, aspectRatio, 0.1f, 100.0f);
+
     gluLookAt(camPos.x, camPos.y, camPos.z,
               lookedAt.x, lookedAt.y, lookedAt.z,
               camUp.x, camUp.y, camUp.z);
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
 }
