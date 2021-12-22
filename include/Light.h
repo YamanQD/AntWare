4e1568e28cbf8d72a43e5a8ed935f137e89429ed
Yaman Qassas <yaman102011@gmail.com>
2021-12-22 23:53:37 +0200

    Add initial Light class

---
+++ include/Light.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <GL/glew.h>
+#include <Transform.h>
+#include <glm/glm.hpp>
+namespace aw
+{
+	class Light
+	{
+	private:
+		unsigned id;
+		bool enabled, isDirectional;
+		glm::vec4 ambient, diffuse, specular;
+		float angle;
+
+	public:
+		Transform transform;
+		Light(unsigned id, glm::vec4 ambient, glm::vec4 diffuse, glm::vec4 specular, glm::vec3 position);									// point light
+		Light(unsigned id, glm::vec4 ambient, glm::vec4 diffuse, glm::vec4 specular, glm::vec3 position, glm::vec3 direction, float angle); // spot light
+		Light(unsigned id, glm::vec4 ambient, glm::vec4 diffuse, glm::vec4 specular, bool isDirectional, glm::vec3 direction);				// directional light
+		void setAmbient(glm::vec4 ambient);
+		void setDiffuse(glm::vec4 diffuse);
+		void setSpecular(glm::vec4 specular);
+		void setAngle(float angle);
+		void update();
+		void toggle();
+	};
+}
---
+++ src/Light.cpp
@@ -0,0 +1,72 @@
+#include <Light.h>
+using namespace aw;
+using namespace glm;
+
+Light::Light(
+	unsigned id,
+	glm::vec4 ambient,
+	glm::vec4 diffuse,
+	glm::vec4 specular,
+	glm::vec3 position) : id(id),
+						  enabled(true),
+						  isDirectional(false),
+						  ambient(ambient),
+						  diffuse(diffuse),
+						  specular(specular),
+						  angle(360.0f)
+{
+	transform = Transform(position, vec3(0, 0, 0), vec3(1, 1, 1));
+}
+Light::Light(
+	unsigned id,
+	glm::vec4 ambient,
+	glm::vec4 diffuse,
+	glm::vec4 specular,
+	glm::vec3 position,
+	glm::vec3 direction,
+	float angle) : id(id),
+				   enabled(true),
+				   isDirectional(false),
+				   ambient(ambient),
+				   diffuse(diffuse),
+				   specular(specular),
+				   angle(angle)
+{
+	if (angle > 360.0f || angle < 0.0f)
+		throw "Light angle must be between 0 and 360 degrees.";
+
+	transform = Transform(position, direction, vec3(1, 1, 1));
+}
+Light::Light(unsigned id, glm::vec4 ambient, glm::vec4 diffuse, glm::vec4 specular, bool isDirectional, glm::vec3 direction)
+{
+	if (!isDirectional)
+		Light(id, ambient, diffuse, specular, direction);
+	else
+	{
+		this->id = id;
+		this->ambient = ambient;
+		this->diffuse = diffuse;
+		this->specular = specular;
+		this->isDirectional = true;
+		transform = Transform(vec3(0, 0, 0), direction, vec3(1, 1, 1));
+	}
+}
+void Light::update()
+{
+	if (isDirectional)
+	{
+		GLfloat light_direction[] = {transform.getRotationAxis().x, transform.getRotationAxis().y, transform.getRotationAxis().z, 0.0f};
+		glLightfv(GL_LIGHT0 + id, GL_POSITION, light_direction);
+	}
+	else
+	{
+		GLfloat light_position[] = {transform.getPosition().x, transform.getPosition().y, transform.getPosition().z, 1.0f};
+		glLightfv(GL_LIGHT0 + id, GL_POSITION, light_position);
+	}
+	GLfloat light_ambient[] = {ambient.r, ambient.g, ambient.b, ambient.a};
+	GLfloat light_diffuse[] = {diffuse.r, diffuse.g, diffuse.b, diffuse.a};
+	GLfloat light_specular[] = {specular.r, specular.g, specular.b, specular.a};
+	glLightfv(GL_LIGHT0 + id, GL_AMBIENT, light_ambient);
+	glLightfv(GL_LIGHT0 + id, GL_DIFFUSE, light_diffuse);
+	glLightfv(GL_LIGHT0 + id, GL_SPECULAR, light_specular);
+}
