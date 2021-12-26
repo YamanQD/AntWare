4915f2ae7fa353c1740b6f0ac223bb74f8ec42d9
Mohammad Issawi <2000issawi@gmail.com>
2021-12-26 21:15:49 +0200

    Revert "Add SoundBuffers class"
    This reverts commit a33e0e91fdfc176c5c90cf569648db4e36365516.

--- src/App.cpp
+++ src/App.cpp
@@ -13,7 +13,6 @@ void App::init(int argc, char **argv)
     readSettingsFile();
     WINDOW.init(settings.resHeight, settings.resWidth);
     RENDERER.init();
-    SoundBuffers::init();
     currentScene = new Scene(argv[1]);
     start();
 }
