4b10c4f3c9f773aabe1a85997fe19e03c48c1358
Yaman Qassas <yaman102011@gmail.com>
2021-12-27 17:28:33 +0200

    Implement quad loadTexture and drawQuad (drawQuad needs fixing)

--- include/HUD.h
+++ include/HUD.h
@@ -1,5 +1,7 @@
 #pragma once
+#include <SFML/Graphics/Image.hpp>
 #include <GL/glew.h>
+#include <glm/glm.hpp>
 #define HUD aw::Hud::instance()
 namespace aw
 {
@@ -7,6 +9,9 @@ namespace aw
     {
     private:
         Hud();
+        GLuint Y;
+        void loadTexture(const char *path);
+        void drawQuad(GLuint texture, glm::vec2 pos, glm::vec2 size);
 
     public:
         static Hud &instance();
--- src/HUD.cpp
+++ src/HUD.cpp
@@ -1,6 +1,11 @@
 #include <HUD.h>
 using namespace aw;
-Hud::Hud() {}
+using namespace sf;
+Hud::Hud()
+{
+    const char *path = "Assets/Textures/Y.png";
+    loadTexture(path);
+}
 Hud &Hud::instance()
 {
     static Hud hud;
@@ -22,6 +27,7 @@ void Hud::draw()
     glLoadIdentity();
 
     // Draw here
+    drawQuad(Y, glm::vec2(8.0f, 4.0f), glm::vec2(4.0f, 4.0f));
 
     glPopMatrix();
     glMatrixMode(GL_PROJECTION);
@@ -30,3 +36,67 @@ void Hud::draw()
     glMatrixMode(GL_MODELVIEW);
     glEnable(GL_DEPTH_TEST);
 }
+void Hud::loadTexture(const char *path)
+{
+    Image image;
+    if (!image.loadFromFile(path))
+    {
+        throw "Couldn't load HUD texture file";
+    }
+    image.flipVertically();
+    int imgHeight = image.getSize().y;
+    int imgWidth = image.getSize().x;
+    const Uint8 *imgData = image.getPixelsPtr();
+
+    glGenTextures(1, &Y);
+    glBindTexture(GL_TEXTURE_2D, Y);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, imgWidth, imgHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, imgData);
+    gluBuild2DMipmaps(GL_TEXTURE_2D, GL_RGBA, imgWidth, imgHeight, GL_RGBA, GL_UNSIGNED_BYTE, imgData);
+}
+void Hud::drawQuad(GLuint texture, glm::vec2 pos, glm::vec2 size)
+{
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glColor3f(1.0f, 1.0f, 1.0f);
+
+    glBegin(GL_QUADS);
+
+    glTexCoord2f(0.0f, 0.0f);
+    glVertex2f(pos.x, pos.y);
+
+    glTexCoord2f(1.0f, 0.0f);
+    glVertex2f(pos.x + size.x, pos.y);
+
+    glTexCoord2f(1.0f, 1.0f);
+    glVertex2f(pos.x + size.x, pos.y + size.y);
+
+    glTexCoord2f(0.0f, 1.0f);
+    glVertex2f(pos.x, pos.y + size.y);
+
+    glEnd();
+    glBindTexture(GL_TEXTURE_2D, 0);
+} // FIXME - Commented code below works, but code above doesn't
+
+// {
+//     glBindTexture(GL_TEXTURE_2D, texture);
+//     glColor3f(1.0f, 1.0f, 1.0f);
+//     glBegin(GL_QUADS);
+//     glTexCoord2f(0.0f, 0.0f);
+//     glVertex2f(-8.0f / 2.0f, -4.5f / 2.0f);
+
+//     glTexCoord2f(1.0f, 0.0f);
+//     glVertex2f(8.0f / 2.0f, -4.5f / 2.0f);
+
+//     glTexCoord2f(1.0f, 1.0f);
+//     glVertex2f(8.0f / 2.0f, 4.5f / 2.0f);
+
+//     glTexCoord2f(0.0f, 1.0f);
+//     glVertex2f(-8.0f / 2.0f, 4.5f / 2.0f);
+//     glEnd();
+//     glBindTexture(GL_TEXTURE_2D, 0);
+// }
