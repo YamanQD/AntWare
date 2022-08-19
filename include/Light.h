d73e45fade7933f0237708f40b08bfcbb5fcfb76
Mohammad Issawi <2000issawi@gmail.com>
2022-08-20 00:03:15 +0300

    Add padding to LightStruct

--- include/Light.h
+++ include/Light.h
@@ -20,6 +20,7 @@ namespace aw
 		{
 			bool enabled;
 			LightType type;
+			float padding0,padding1;
 			glm::vec4 ambient, diffuse, specular;
 			glm::vec3 direction;
 			float angle;
@@ -32,9 +33,9 @@ namespace aw
 
 	public:
 		Transform transform;
-		Light( glm::vec4 ambient, glm::vec4 diffuse, glm::vec4 specular, glm::vec3 position, GameObject *parent = nullptr);									  // point light
-		Light( glm::vec4 ambient, glm::vec4 diffuse, glm::vec4 specular, glm::vec3 position, glm::vec3 direction, float angle, GameObject *parent = nullptr); // spot light
-		Light( glm::vec4 ambient, glm::vec4 diffuse, glm::vec4 specular, LightType type, glm::vec3 direction, GameObject *parent = nullptr);				  // directional light
+		Light(glm::vec4 ambient, glm::vec4 diffuse, glm::vec4 specular, glm::vec3 position, GameObject *parent = nullptr);									 // point light
+		Light(glm::vec4 ambient, glm::vec4 diffuse, glm::vec4 specular, glm::vec3 position, glm::vec3 direction, float angle, GameObject *parent = nullptr); // spot light
+		Light(glm::vec4 ambient, glm::vec4 diffuse, glm::vec4 specular, LightType type, glm::vec3 direction, GameObject *parent = nullptr);					 // directional light
 		~Light();
 		void setAmbient(glm::vec4 ambient);
 		void setDiffuse(glm::vec4 diffuse);
@@ -43,6 +44,6 @@ namespace aw
 		void update();
 		void toggle();
 
-		static void constructUniformBuffer( std::vector<Light> &lights);
+		static void constructUniformBuffer(std::vector<Light> &lights);
 	};
 }
