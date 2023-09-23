8dc78a559d5800ae316f656cca9ffbbcdf440587
Mohammad Issawi <2000issawi@gmail.com>
2023-09-24 01:36:42 +0300

    Document helpers.h file

--- include/helpers.h
+++ include/helpers.h
@@ -1,7 +1,15 @@
+/** \file helpers.h
+ *  \brief A set of general helper functions.
+ */
 #pragma once
-#include<fstream>
-#include<vector>
-#include<assert.h>
-namespace aw{
+#include <fstream>
+#include <vector>
+#include <assert.h>
+namespace aw
+{
+    /** \brief Reads a binary file.
+     * \param path The path of the binary file to be read.
+     * \return The bytes of the file specified in path.
+     */
     std::vector<char> readBinFile(const char *path);
 }
