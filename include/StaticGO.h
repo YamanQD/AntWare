a1539ee1385f7f2eb5afad157e5ed0722ccacb98
Mohammad Issawi <2000issawi@gmail.com>
2023-09-29 01:19:40 +0300

    Document StaticGO class

--- include/StaticGO.h
+++ include/StaticGO.h
@@ -1,12 +1,19 @@
 #pragma once
 #include <GameObject.h>
-namespace aw
-{
-    class StaticGO : public GameObject
-    {
-    public:
-        StaticGO(std::shared_ptr<Mesh> mesh, Material material, GameObject *parent = nullptr);
-        void start() override;
-        void update() override;
-    };
-}
+namespace aw {
+/** \brief Basic GameObject, just a standing still Meshes, used for decorations
+ * mostly.*/
+class StaticGO : public GameObject {
+public:
+  /** \param mesh The GameObject's Mesh.
+   * \param material The GameObject's Material.
+   * \param parent The GameObject's parent.
+   */
+  StaticGO(std::shared_ptr<Mesh> mesh, Material material,
+           GameObject *parent = nullptr);
+  /** \brief Sets ::isStatic to true.*/
+  void start() override;
+  /** \brief Does nothing.*/
+  void update() override;
+};
+} // namespace aw
