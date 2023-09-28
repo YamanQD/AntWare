13e0a80e1ba1d22b9ddea25996bcb1d84b4af5cb
Mohammad Issawi <2000issawi@gmail.com>
2023-09-28 22:34:04 +0300

    Document Camera class

--- include/Camera.h
+++ include/Camera.h
@@ -6,25 +6,56 @@
 #include <GameObject.h>
 namespace aw
 {
-    class Camera
-    {
-    private:
-        float FOV;
-        static float aspectRatio;
-        glm::mat4 viewPerspective;
-        GLint viewPerspectiveLocation,posLocation;
+/** \brief %Camera abstraction, simulates a perspective view using perspective
+ * matrix multiplication.*/
+class Camera {
+private:
+  /** \brief The field-of-View value, in radians.*/
+  float FOV;
+  /** \brief The aspect ratio of the camera frustum, matches the window's
+   * for
+   * best resutlts.*/
+  static float aspectRatio;
+  /** \brief The transformation matrix from world space to perspective
+   * camera
+   * space.*/
+  glm::mat4 viewPerspective;
+  GLint viewPerspectiveLocation, posLocation;
 
-    public:
-        Transform transform;
-        Rigidbody rigidbody;
+public:
+  Transform transform;
+  Rigidbody rigidbody;
 
-        Camera(float FOV);
-        void setFOV(float FOV, bool doUpdate = true);
-        void setAspectRatio(float aspectRatio);
-        // Call to update camera matrices
-        void update();
-        void fixedUpdate(float deltaTime);
-        void setViewPerspectiveLocation(GLint location);
-        void setPosLoaction(GLint location);
-    };
+  /** \brief Calculates ::aspectRatio using Window size.
+   *
+   * \param FOV Overwrites ::FOV.
+   */
+  Camera(float FOV);
+  /** \param FOV overwrites ::FOV.
+   * \param doUpdate call ::update if true, do nothing otherwise.
+   */
+  void setFOV(float FOV, bool doUpdate = true);
+  /** \brief Updates ::aspectRatio and calls ::update.
+   *
+   * \param aspectRatio Overwrites ::aspectRatio.
+   */
+  void setAspectRatio(float aspectRatio);
+  /** \brief Updates VP and observerPos variables in Shaders/main.vert and
+   * Shaders/main.frag using viewPerspective and Transform::getPosition, in that
+   * order.
+   */
+  void update();
+  /** \brief Updates the physics properties and the transformation based
+   * on
+   * ::rigidbody data.*/
+  void fixedUpdate(float deltaTime);
+  /** \param location Overwrites ::viewPerspectiveLocation, the handle of the VP
+   * matrix in Shaders/main.vert.
+   */
+  void setViewPerspectiveLocation(GLint location);
+  /** \param location Overwrites ::posLocation, the handle of the observerPos
+   * vector in Shaders/main.frag.
+   */
+  void setPosLoaction(GLint location);
+};
 }
